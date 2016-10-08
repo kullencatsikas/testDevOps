64564dde920768461533ba74c2b41b3db7a712d2
kullencatsikas <kullencatsikas@gmail.com>
2016-10-07 20:27:36 -0400

    search boyhood

--- TestApp/src/test/java/TestGoogle.java
+++ TestApp/src/test/java/TestGoogle.java
@@ -10,18 +10,16 @@ public class TestGoogle extends TestBase {
 
     @Test
 
-    public synchronized void goToGoogle() {
+    public  void goToGoogle() {
 
         driver.get("http://www.google.com");
         WebElement searchBar  = driver.findElement(By.id("lst-ib"));
         searchBar.sendKeys("boyhood");
         WebElement searchButton = driver.findElement(By.className("sbico"));
         searchButton.click();
+
         try {
-
-                driver.wait(5);
-
-
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
